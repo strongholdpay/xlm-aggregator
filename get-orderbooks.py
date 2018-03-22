22e8be67e0374f1b6b67fb01d33922db3a08032e
Tammy Camp <tammy@tammycamp.com>
2018-03-22 14:16:15 -0700

    added ccxt exchanges

--- get-orderbooks.py
+++ get-orderbooks.py
@@ -1,79 +1,54 @@
 
 # In terminal need to:
 # pip install requests
-
 # make a bot.py and put this code in
 # https://github.com/ccxt/ccxt/tree/master/python standardized exchange data parsing
 import os
-import requests
 import ccxt
 
 
-def get_order_book(url):
-	response = requests.get(url)
-	data = {}
-	if response.status_code != 200:
-		print('Error! {}'.format(response.status_code))
-	else:
-		data = response.json()
-	return data
+def print_results(exchange_name, results):
+	query_date = results['datetime']
 
+	print('-----bids-----')
+	for result in results['bids']:
+		print([result[0], result[1], 'bid', exchange_name, query_date])
 
-def parse_binance_data(exchange_name, raw_data):
-	print(raw_data)
-	print(raw_data.keys())
-	# dict_keys(['lastUpdateId', 'bids', 'asks' 'price', 'qty'])
-	bids = raw_data['bids']
-	print(bids)
-	ask_price = raw_data['']
-	bid_price = raw_data['']
-	order_size = raw_data['']
-	volume = raw_data['']
-	ticker_symbol_xlm = raw_data['']
-	ticker_symbol_btc = raw_data['']
-	timestamp = raw_data['']
-	return [exchange_name, ask_price, bid_price, order_size, ticker_symbol_xlm, ticker_symbol_btc, timestamp]
+	print('-----asks-----')
+	for result in results['asks']:
+		print([result[0], result[1], 'ask', exchange_name, query_date])
 
-def parse_bittrex_data(exchange_name, raw_data):
-	bid_data = raw_data['result']['buy']
-	print(bid_data)
-	# {'Quantity': 834.72615344, 'Rate': 2.652e-05}
-	#print(bid_data.keys())
-	# dict_keys(['lastUpdateId', 'bids', 'asks'])
-	result_data = []
-	for bid in bid_data:
-		row_data = [exchange_name, None, bid['Rate'], bid['Quantity'], None, None, None, None]
-		result_data.append(row_data)
-	return result_data
-	# All price data is in BTC
 
 
 def main():
 	exchanges = [
-		#('https://www.binance.com/api/v1/depth?symbol=XLMBTC', 'binance', parse_binance_data),
-		('https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-XLM&type=both', 'bittrex', parse_bittrex_data),
-		#('https://api.hitbtc.com/api/2/public/orderbook/xlmbtc', 'hitbtc', parse_hitbtc_data),
-		#('https://api.kraken.com/0/public/Depth?pair=XLMXBT', 'kraken', parse_kraken_data),
-		#('https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_STR', 'poloniex', parse_poloniex_data),
+		'binance',
+		'bittrex',
+		'bitcoincoid',
+		'cex',
+		'hitbtc',
+		'kraken',
+		'poloniex',
 
 	]
-	for exchange in exchanges:
-		url = exchange[0]
-		exchange_name = exchange[1]
-		parsing_function = exchange[2]
-		raw_data = get_order_book(url)
-		parsed_data = parsing_function(exchange_name, raw_data)
-		print(parsed_data)
+	for exchange_name in exchanges:
+		constructor = getattr(ccxt, exchange_name)
+		exchange = constructor()
+		results = exchange.fetch_order_book('XLM/BTC')
+		#print(results.keys())
+		# dict_keys(['bids', 'asks', 'timestamp', 'datetime'])
+		#print(results['datetime'])
 		#print(data)
+		print_results(exchange_name, results)
+		#break
 
-	print(ccxt.exchanges)
+	#print(exchange.fetchMarkets())
 
 
 if __name__ == "__main__":
 	main()
 
 
-# Ask Price, Bid Price, Order Size, Volume, Ticker Symbol XLM, Ticket Symbol BTC, Exchange Name, Timestamp
-
+# Ask Price, Bid Price, Order Size, Ticker Symbol XLM, Ticket Symbol BTC, Exchange Name, Timestamp
 # Bid = Buy
 # Ask = Sell
