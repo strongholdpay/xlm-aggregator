74780348efd387d03fa49743c4187616c894a6c7
Tammy Camp <tammy@tammycamp.com>
2018-04-09 10:08:10 -0700

    added models + database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+engine = create_engine('postgresql://localhost:5432/stronghold-sandbox', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -13,5 +13,5 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import yourapplication.models
+    import models
     Base.metadata.create_all(bind=engine)
--- models.py
+++ models.py
@@ -1,12 +1,19 @@
 import datetime
 import enum
-from sqlalchemy import Column, DateTime, Enum, Integer, Numeric, String
-from yourapplication.database import Base
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, Numeric, String
+from database import Base
+
+
+class Exchange(Base):
+	__tablename__ = 'exchanges'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(50))
 
 
 class OrderTypeEnum(enum.Enum):
 	buy = 1
 	sell = 2
+
 
 class Order(Base):
     __tablename__ = 'users'
@@ -14,7 +21,7 @@ class Order(Base):
     order_type = Column(Enum(OrderTypeEnum))
     quantity = Column(Numeric(8,8))
     price = Column(Numeric(8,8))
-    exchange = Column(String(50))
+    exchange_id = Column(Integer, ForeignKey('exchanges.id'))
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
 
     def __init__(self, order_type=None, quantity=None, price=None, exchange=None):
